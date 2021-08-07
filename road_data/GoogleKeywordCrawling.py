293e0b77c540ebb537e70df345bbae104418c748
shindonghwi <ehdgnl8940@naver.com>
2021-08-07 19:17:35 +0900

    feat: 이름, 서브이름, 시간, 주소 가져오기 성공

--- road_data/GoogleKeywordCrawling.py
+++ road_data/GoogleKeywordCrawling.py
@@ -1,12 +1,19 @@
 import random
+import time
 
+from selenium.common.exceptions import NoSuchElementException
+import requests
+from bs4 import BeautifulSoup
 import pymysql as pymysql
 from selenium import webdriver
 import pymysql as pymysql
 import os
 from tqdm import tqdm
 import pyautogui
+from mechanize import Browser
+import threading
 
+myTimer = 0
 dbConnect = None
 dbCursor = None
 driver = None
@@ -32,11 +39,11 @@ def connectDB():
 
 
 def getKeywordList():
-    print("키워드 리스트 가져오기 시작")
+    # print("키워드 리스트 가져오기 시작")
     sql = "select `keyword` from `Keyword`"
     dbCursor.execute(sql)
     keywordList = [item[0] for item in dbCursor.fetchall()]
-    print("키워드 리스트 가져오기 끝 : ", keywordList, "\n")
+    # print("키워드 리스트 가져오기 끝 : ", keywordList, "\n")
     return keywordList
 
 
@@ -44,11 +51,11 @@ def getKeywordList():
 
 
 def getRegionList():
-    print("지역 리스트 가져오기 시작")
+    # print("지역 리스트 가져오기 시작")
     sql = "select `region` from `Region`"
     dbCursor.execute(sql)
     regionList = [item[0] for item in dbCursor.fetchall()]
-    print("지역 리스트 가져오기 끝 : ", regionList, "\n")
+    # print("지역 리스트 가져오기 끝 : ", regionList, "\n")
     return regionList
 
 
@@ -56,11 +63,11 @@ def getRegionList():
 
 
 def getRoadList():
-    print("도로명주소 리스트 가져오기 시작")
+    # print("도로명주소 리스트 가져오기 시작")
     sql = "select `region_idx`, `road_name` from `RoadName`"
     dbCursor.execute(sql)
     roadList = [item for item in dbCursor.fetchall()]
-    print("도로명주소 리스트 가져오기 끝 : ", roadList, "\n")
+    # print("도로명주소 리스트 가져오기 끝 : ", roadList, "\n")
     return roadList
 
 
@@ -89,26 +96,35 @@ def searchDataInputAndClick():
     driver.find_element_by_id('searchboxinput').send_keys(searchInputData)
     driver.find_element_by_xpath('//*[@id="searchbox-searchbutton"]').click()
 
+def myTimerFuntion():
+    global myTimer
+    myTimer += 1
+    timer = threading.Timer(1, myTimerFuntion)
+    timer.start()
+
+    if myTimer == 5:
+        timer.cancel()
 
 """ 콘텐츠를 가장 아래까지 스크롤"""
 
 
 def contentDownScrolling():
-    global driver
-    isContentShowing = False
+    global driver, myTimer
+
+    myTimerFuntion()
     while True:
+        if myTimer == 5:
+            myTimer = 0
+            break
+
         try:
-            isContentShowing = driver.find_element_by_xpath(
-                '//*[@id="pane"]/div/div[1]/div/div/div[2]/div[2]/div/button/span[1]').is_displayed()
-            print(isContentShowing)
-            if isContentShowing:
-                driver.implicitly_wait(0.5)
-                break
-        except:
+            driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div/button/span[1]').is_displayed()
+            time.sleep(0.5)
+            break
+        except Exception as e:
             pass
 
     animation = [pyautogui.easeInQuad, pyautogui.easeInBounce, pyautogui.easeInCubic]
-    whileCount = 0
     while True:
         pyautogui.moveTo(
             random.randint(50, 400),
@@ -117,13 +133,81 @@ def contentDownScrolling():
             animation[random.randint(0, 2)]
         )
         pyautogui.scroll(-800)
-        driver.implicitly_wait(3)
-        whileCount += 1
-        if whileCount == 6:
+
+        try:
+            driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[4]/div[1]/div[39]/div').is_selected()
             break
+        except Exception as e:
+            pass
+
+
+def contentClick(contentIdx):
+    global driver
+    driver.find_element_by_xpath(
+        '//*[@id="pane"]/div/div[1]/div/div/div[4]/div[1]/div[' + str(contentIdx) + ']/div/a').click()
+
+
+def getPlaceInfo():
+    global driver
+
+    while True:
+        try:
+            driver.find_element_by_xpath(
+                '//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[1]/h1/span[1]').is_displayed()
+            break
+        except:
+            pass
+
+    name = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[1]/h1/span[1]')
+    print(name.text)
+
+    sub_name = driver.find_element_by_xpath(
+        '//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/span[1]/span[1]/button')
+    print(sub_name.text)
+
+    address = driver.find_element_by_xpath(
+        '//*[@id="pane"]/div/div[1]/div/div/div[7]/div[1]/button/div[1]/div[2]/div[1]').text
+    print(address)
+
+    # 장소 place_gm_blue_24dp
+    # 시간 schedule_gm_blue_24dp
+    # 사이트 public_gm_blue_24dp
+    # 번호 phone_gm_blue_24dp
+
+    try:
+        for i in range(2, 5):
+            try:
+                imageType = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[7]/div[' + str(
+                    i) + ']/button/div[1]/div[1]/div/img').get_attribute('src')
+            except:
+                imageType = driver.find_element_by_xpath(
+                    '//*[@id="pane"]/div/div[1]/div/div/div[7]/div[' + str(i) + ']/div[1]/img').get_attribute('src')
+
+            print(imageType)
+
+            if "place_gm_blue_24dp" in imageType:
+                # 장소
+                pass
+            # 시간
+            elif "schedule_gm_blue_24dp" in imageType:
+                driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[7]/div['+str(i)+']/div[1]/div/div[1]/span[2]').click()
+                timeInfo = driver.find_element_by_xpath('//*[@id="pane"]/div/div[1]/div/div/div[7]/div[' + str(i) + ']/div[2]')
+                print(timeInfo.text)
+            elif "public_gm_blue_24dp" in imageType:
+                # 시간
+                pass
+            elif "phone_gm_blue_24dp" in imageType:
+                # 시간
+                pass
+
+    except NoSuchElementException as e:
+        pass
+
+    driver.find_element_by_xpath('//*[@id="omnibox-singlebox"]/div[1]/div[4]/div/div[1]/div/div/button/span').click()
+    time.sleep(2)
+
 
 if __name__ == '__main__':
-
     connectDB()
     regionList = getRegionList()
     roadList = getRoadList()
@@ -143,14 +227,15 @@ if __name__ == '__main__':
 
             for keyword in keywordList:  # 필라테스, 태권도 ~~
 
-                searchDataInputAndClick() # """ 검색어 입력후 클릭 """
-                contentDownScrolling() # """ 콘텐츠를 가장 아래쪽까지 스크롤 """
+                searchDataInputAndClick()  # """ 검색어 입력후 클릭 """
+                contentDownScrolling()  # """ 콘텐츠를 가장 아래쪽까지 스크롤 """
+
+                for contentIdx in range(1, 40, 2):
+                    print(contentIdx)
+                    if contentIdx >= 13:
+                        contentDownScrolling()
+
+                    contentClick(contentIdx)  # 콘텐츠 클릭
+                    getPlaceInfo()  # 콘텐츠 정보 수집
 
                 exit()
-
-# browser.find_element_by_xpath('//*[@id="lu_map"]').click()
-#
-# gpsInfo = str(browser.current_url).split('/')[6].replace("@","").split(",")
-# latitude = gpsInfo[0]
-# longitude = gpsInfo[1]
-# print(latitude, longitude)
